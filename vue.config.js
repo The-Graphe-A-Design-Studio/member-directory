cd01a3a54095c3b01ff0633fe454666e3d3224dc
Sanny67 <sayantanichatterjee67@gmail.com>
2020-09-03 22:48:36 +0530

    public path changed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'https://developers.thegraphe.com'
-        ? '/member-directory/'
-        : '/'
+    publicPath: '/member-directory/'
 }

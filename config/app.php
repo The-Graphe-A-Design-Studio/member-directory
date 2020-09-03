eb253227b754a9332159621a323d2e2842e6c992
Sanny67 <sayantanichatterjee67@gmail.com>
2020-09-03 20:27:20 +0530

    config url changed

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://developers.thegraphe.com/member-directory'),
+    'url' => 'https://developers.thegraphe.com/member-directory',
 
     'asset_url' => env('ASSET_URL', null),
 

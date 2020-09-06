586d3259e56bdcd434675266ce737fcd882b6c1b
Sanny67 <sayantanichatterjee67@gmail.com>
2020-09-06 20:52:27 +0530

    giving up and putting hash in admin routes

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
 
-Route::prefix('/admin')->group( function(){
+Route::prefix('/#/admin')->group( function(){
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
     Route::get('', 'AdminController@index')->name('admin.dashboard');
